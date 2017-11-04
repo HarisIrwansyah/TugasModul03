bc1eae342760f0b1beb6a7a9609569665ac88837
HarisIrwansyah <haris.irwansyah4@gmail.com>
2017-11-04 21:48:28 +0800

    Menginisialisasikan Inputan menghitung Gaji Bersih

--- Soal03.cpp
+++ Soal03.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 
 int main () {
+	int JK, LEMBUR, GTOT, GP, GB, UL, JAM, PAJAK ;
 	
+	cout << "Lama Kerja Karyawan dalam Seminggu              : "; cin >> JK ;
+	cout << "Gaji Perjam yang Karyawan Terima dalam Seminggu : "; cin >> GP ;	
+
 }
