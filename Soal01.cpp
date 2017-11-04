f0259910beb82e6e2eef3e6da1c756c671f0aab0
HarisIrwansyah <haris.irwansyah4@gmail.com>
2017-11-04 21:33:15 +0800

    Menginisialisasi Nilai yang di input jika X>Y

--- Soal01.cpp
+++ Soal01.cpp
@@ -7,4 +7,9 @@ int main () {
 	cout << "Masukkan Nilai X : "; cin >> X ;
 	cout << "Masukkan Nilai Y : "; cin >> Y ;
 
+	if (X>Y){
+		Z = X-Y;
+		
+		cout << "Selisih Nilai X dan Y yaitu "<< Z ;
+	}
 }
